2b33ade7641848c9e8f89e85a8ef3517b49ef695
Nycola Plaisance <nPlaisance@recursyve.io>
2025-10-03 11:16:05 -0400

    feat: Implement swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	const app = await NestFactory.create(AppModule);
+
+	const config = new DocumentBuilder()
+		.setTitle("Sorty API")
+		.setDescription("API that powers the Sorty platform")
+		.setVersion(process.env.VERSION ?? "")
+		.build();
+	const documentFactory = () => SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup("api", app, documentFactory);
+
+	await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
